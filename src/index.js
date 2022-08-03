de229df4b585371d217205cac134f5fa9c0747c4
BenRoumagne <110542829+BenRoumagne@users.noreply.github.com>
2022-08-03 16:19:46 +0200

    manip form object

--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
+const dom = document.getElementById("app");
+
+//console.log(dom);
+
+var form = {
+  for: "toto",
+  array: ["plop", "plop2"],
+  id: "id",
+  type: "type"
+};
+console.log(form.array[0]);
+dom.innerHTML =
+  `
 <h1>Hello Truffe!</h1>
+
 <div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+
+<form action="" method="get" class="form-example">
+  <div class="form-example">
+  <label ` +
+  form.for +
+  `=` +
+  form.array[0] +
+  `>Enter your name: </label>
+    <input type="text" name="name" id="name" required>
+  </div>
+  <div class="form-example">
+    <label ` +
+  form.for +
+  `="email">Enter your email: </label>
+    <input type="email" name="email" id="email" required>
+  </div>
+  <div class="form-example">
+    <input type="submit" value="Subscribe!">
+  </div>
+</form>
+
 </div>
+
 `;
