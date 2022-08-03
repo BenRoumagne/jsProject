17da126fca51f4462ff3000c8de374dc4d52c005
BenRoumagne <110542829+BenRoumagne@users.noreply.github.com>
2022-08-03 16:56:32 +0200

    map js test

--- src/index.js
+++ src/index.js
@@ -6,36 +6,22 @@ const dom = document.getElementById("app");
 
 var form = {
   for: "toto",
-  array: ["plop", "plop2"],
+  array: ["plop", "plop2", "test", "tttttt", "rrrrrr"],
   id: "id",
   type: "type"
 };
-console.log(form.array[0]);
+//console.log(form.array[0]);
 dom.innerHTML =
   `
 <h1>Hello Truffe!</h1>
 
 <div>
 
-<form action="" method="get" class="form-example">
-  <div class="form-example">
-  <label ` +
-  form.for +
-  `=` +
-  form.array[0] +
-  `>Enter your name: </label>
-    <input type="text" name="name" id="name" required>
-  </div>
-  <div class="form-example">
-    <label ` +
-  form.for +
-  `="email">Enter your email: </label>
-    <input type="email" name="email" id="email" required>
-  </div>
-  <div class="form-example">
-    <input type="submit" value="Subscribe!">
-  </div>
-</form>
+<ul>` +
+  form.array.map((value) => {
+    return "<li>" + value + "</li>";
+  }) +
+  `</ul>
 
 </div>
 
